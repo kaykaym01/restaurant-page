889435ad8d5eb7e261f96d0529b380fd87b5659b
kaykaym01 <kaymoore01@gmail.com>
2022-08-05 22:02:52 -0400

    Add content render for menu page

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,12 +1,79 @@
+import menuItems from "./menuItems";
+import '../css/menu.css';
+
+
 /**
  * Adds content to and returns menu
  * @returns Site menu page
  */
 function loadMenu() {
+    console.log("menu loaded");
+    return menu();
+}
+
+/**
+ * Renders content and returns menu
+ * @returns menu div
+ */
+function menu() {
     const menu = document.createElement("div");
     menu.classList.add("menu");
-    console.log("menu loaded");
+
+    const menuTitle = document.createElement("h1");
+    menuTitle.classList.add("page-title")
+    menuTitle.textContent = "Menu";
+    menu.appendChild(menuTitle);
+
+    menuItems.forEach(menuType => {
+        menu.appendChild(submenu(menuType));
+    });
     return menu;
 }
+
+/**
+ * Renders the content for the submenu including the title and items
+ * @param {*} menu The submenu to be rendered
+ * @returns The submenu content
+ */
+function submenu(menu) {
+    const subMenu = document.createElement("div");
+    subMenu.classList.add("submenu");
+    const submenuTitle = document.createElement("h2");
+    submenuTitle.classList.add("submenu-title");
+    submenuTitle.textContent = menu.type;
+    subMenu.appendChild(submenuTitle);
+    subMenu.appendChild(submenuItems(menu.items));
+    return subMenu;
+}
+
+/**
+ * Renders the list of items and prices for the sub menu 
+ * @param {*} items The list of items and prices to be rendered
+ * @returns The items content
+ */
+function submenuItems(items){
+    const submenuItems = document.createElement("ul");
+    submenuItems.classList.add("submenu-items");
+
+    items.forEach(item => {
+        const itemRow = document.createElement('li');
+        itemRow.classList.add("item-row");
+
+        const itemName = document.createElement('p');
+        itemName.classList.add('item-name');
+        itemName.textContent = item.name;
+        itemRow.appendChild(itemName);
+
+        const itemPrice = document.createElement('p');
+        itemPrice.classList.add('item-price');
+        itemPrice.textContent = item.price;
+        itemRow.appendChild(itemPrice);
+        submenuItems.append(itemRow);
+    });
+    return submenuItems;
+}
+
+
+
 
 export default loadMenu;
---
+++ src/pages/menuItems.js
@@ -0,0 +1,68 @@
+/**
+ * Returns a list of menu items by type
+ * @returns list of menu items by type
+ */
+const menuItems = [
+        {
+            type: 'Mains',
+            items: [
+                {
+                    name: 'Curried Vegetable Galette',
+                    price: '17.99'
+                },
+                {
+                    name: 'Sauteed Shrimp and Okra',
+                    price: '23.99'
+                },
+                {
+                    name: 'Fried Chicken',
+                    price: '18.99'
+                },
+                {
+                    name: 'Roasted Fish and Gravy',
+                    price: '25.99'
+                },
+                {
+                    name: 'Fried Corn Cakes',
+                    price: '15.99'
+                },
+            ]
+        },
+        {
+            type: 'Sides',
+            items: [
+                {
+                    name: 'Gullah Rice',
+                    price: '9.99'
+                },
+                {
+                    name: 'Collard Greens',
+                    price: '8.99'
+                },
+                {
+                    name: 'Mac and Cheese',
+                    price: '11.99'
+                },
+                {
+                    name: 'Corn Bread',
+                    price: '6.99'
+                },
+            ]
+        },
+        {
+            type: 'Desserts',
+            items: [
+                {
+                    name: 'Peaches Cobbler',
+                    price: '8.99'
+                },
+                {
+                    name: 'Bread Pudding',
+                    price: '7.99'
+                },
+            ]
+        }
+    ];
+
+export default menuItems;
+
