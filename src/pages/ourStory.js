4117e01a46411a82ca4cbf8cf266ccb8dede5af4
kaykaym01 <kaymoore01@gmail.com>
2022-08-10 19:17:18 -0400

    Add title to Our Story page

--- src/pages/ourStory.js
+++ src/pages/ourStory.js
@@ -3,10 +3,21 @@
  * @returns Site our story page
  */
 function loadOurStory() {
+    return ourStory();
+}
+
+function ourStory(){
     const ourStory = document.createElement('div');
     ourStory.classList.add("our-story");
-    console.log("our story loaded");
-    return ourStory
+    const ourStoryTitle = document.createElement('h1');
+    ourStoryTitle.classList.add('page-title');
+    ourStoryTitle.textContent = "Our Story";
+    ourStory.appendChild(ourStoryTitle);
+    return ourStory;
+}
+
+function ourStoryText(){
+    
 }
 
 export default loadOurStory;
