677060f6f2077a91c2152b8f9fa33265abfdde3f
kaykaym01 <kaymoore01@gmail.com>
2022-08-10 19:16:42 -0400

    Add title to Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,9 +3,17 @@
  * @returns Site contact page
  */
 function loadContact() {
+        return contact();
+}
+
+function contact() {
     const contact = document.createElement('div');
     contact.classList.add("contact");
-    console.log("contact loaded");
+
+    const contactTitle = document.createElement("h1");
+    contactTitle.classList.add("page-title")
+    contactTitle.textContent = "Contact Us";
+    contact.appendChild(contactTitle);
     return contact;
 }
 
