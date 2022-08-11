aa1741fd65dae6cf6cbae252db182cedc5fc002f
kaykaym01 <kaymoore01@gmail.com>
2022-08-10 21:34:03 -0400

    Add ourStory content and layout

--- src/pages/ourStory.js
+++ src/pages/ourStory.js
@@ -1,3 +1,6 @@
+import storyContent from "./storyContent";
+import '../css/ourStory.css';
+
 /**
  * Adds content to and returns our story
  * @returns Site our story page
@@ -13,11 +16,16 @@ function ourStory(){
     ourStoryTitle.classList.add('page-title');
     ourStoryTitle.textContent = "Our Story";
     ourStory.appendChild(ourStoryTitle);
+
+    ourStory.appendChild(ourStoryText());
     return ourStory;
 }
 
 function ourStoryText(){
-    
+    const storyText = document.createElement('p');
+    storyText.classList.add("story-content");
+    storyText.textContent = storyContent;
+    return storyText;
 }
 
 export default loadOurStory;
---
+++ src/pages/storyContent.js
@@ -0,0 +1,9 @@
+const storyContent = 'Off the coasts of North Carolina, South Carolina, Georgia, and \
+Florida, our enslaved ancestors formed an entirely unique culture that was a mix of West\
+and Central African, as well as Native American. Using the ingredients that were available\
+in Lowcountry, such as crabs, rice, okra, grits, and others, they created what has become the \
+roots of most Southern dishes like Fried Chicken and Shrimp and Grits. We hope that  through \
+our sharing of these dishes, you\'ll come to further appreciate and understand Gullah Geechee \
+history and culture.';
+
+export default storyContent;
