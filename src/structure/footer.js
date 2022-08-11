8ab92a4932cfa28705273666c56e25357f8e9f5d
kaykaym01 <kaymoore01@gmail.com>
2022-08-10 22:33:23 -0400

    Add content to site footer

--- src/structure/footer.js
+++ src/structure/footer.js
@@ -3,8 +3,17 @@
  * @returns Site footer
  */
 function loadFooter() {    
+ 
+    return footer();
+}
+
+function footer(){
     const footer = document.createElement("footer");
-    footer.classList.add("footer");  
+    footer.classList.add("footer"); 
+    const copyrightMessage = document.createElement("p");
+    copyrightMessage.textContent = "Rooted. 2022. All Rights Reserved";
+    footer.appendChild(copyrightMessage);
+    copyrightMessage.style.textAlign = "center";
     return footer;
 }
 
