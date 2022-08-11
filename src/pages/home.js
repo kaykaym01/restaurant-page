0165c62a1093f29f81fefed0404cb0e0127beb65
kaykaym01 <kaymoore01@gmail.com>
2022-08-10 20:34:32 -0400

    Add home page content and styling

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,30 @@
+import '../css/home.css';
+
 /**
  * Adds content to and returns home
  * @returns Site home page
  */
 function loadHome() {
+    return home();
+}
+
+function home(){
     const home = document.createElement("div");
     home.classList.add("home");
-    console.log("home loaded");
+    home.appendChild(topContent());
     return home;
+}
+
+function topContent(){
+    const topContent = document.createElement("div");
+    topContent.classList.add("top-content");
+
+    const headline = document.createElement("h2");
+    headline.classList.add("headline");
+    headline.textContent = "southern cuisine with roots in gullah geechee tradition";
+    topContent.appendChild(headline);
+
+    return topContent;
 }
 
 export default loadHome;
