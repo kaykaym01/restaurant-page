503f512694d904518b3b570a663bc4702969f78e
kaykaym01 <kaymoore01@gmail.com>
2022-09-15 23:44:31 -0400

    Add webpack dev server for local deployment

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,11 @@ module.exports = {
     clean: true,
     publicPath: '',
   },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 8080,
+    hot: true
+  },
   module: {
     rules: [
       {
